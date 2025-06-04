7f7bc535e77b19d82be56f246ebcfae2e3438359
Jonier Pachon <jhonier.pachon1015@gmail.com>
2025-06-04 07:06:58 -0500

    Enhance Button Functionality: Implement All, Active, and inactive States.

--- script.js
+++ script.js
@@ -1,39 +1,91 @@
 // dynamic data
 
-fetch("./data.json")
-   .then((res) => res.json())
-   .then((data) => {
-      //
-      const container = document.querySelector("#extensions-container");
-      //
-      data.forEach((extension) => {
-         const card = document.createElement("div");
-         card.className = "card";
+function getDynamicData(filter) {
+   fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => {
+         const container = document.querySelector("#extensions-container");
+         container.innerHTML = "";
+         data.forEach((extension) => {
+            const card = document.createElement("div");
+            card.className = "card";
 
-         card.innerHTML = `
-         <div class="first-part">
-            <img src="${extension.logo}" alt="${
-            extension.name
-         } logo" class="logo" />
-            <div class="text">
-               <h2>${extension.name}</h2>
-               <p>${extension.description}</p>
-            </div>
-         </div>
-         <div class="second-part">
-            <button class="btn-remove">Remove</button>
-            <label class="switch">
-               <input type="checkbox" ${extension.isActive ? "checked" : ""} />
-               <span class="slider round" tabindex="0"></span>
-            </label>
-         </div>
-         
-            `;
+            if (filter === "all") {
+               card.innerHTML = displayData(extension);
+               container.appendChild(card);
+            } else if (filter === "active" && extension.isActive) {
+               card.innerHTML = displayData(extension);
+               container.appendChild(card);
+            } else {
+               if (filter === "inactive" && !extension.isActive) {
+                  card.innerHTML = displayData(extension);
+                  container.appendChild(card);
+               }
+            }
+         });
+         function displayData(extension) {
+            let data = `
+               <div class="first-part">
+                  <img src="${extension.logo}" alt="${
+               extension.name
+            } logo" class="logo" />
+                  <div class="text">
+                     <h2>${extension.name}</h2>
+                     <p>${extension.description}</p>
+                  </div>
+               </div>
+               <div class="second-part">
+                  <button class="btn-remove">Remove</button>
+                  <label class="switch">
+                     <input type="checkbox" ${
+                        extension.isActive ? "checked" : ""
+                     } />
+                     <span class="slider round" tabindex="0"></span>
+                  </label>
+               </div>
+               `;
+            return data;
+         }
+      })
+      .catch((err) => console.error("Failed to load JSON", err));
+}
 
-         container.appendChild(card);
-      });
-   })
-   .catch((err) => console.error("Failed to load JSON", err));
+getDynamicData("all");
+
+// fetch("./data.json")
+//    .then((res) => res.json())
+//    .then((data) => {
+//       //
+//       const container = document.querySelector("#extensions-container");
+//       //
+//       data.forEach((extension) => {
+//          const card = document.createElement("div");
+//          card.className = "card";
+
+//          card.innerHTML = `
+//          <div class="first-part">
+//             <img src="${extension.logo}" alt="${
+//             extension.name
+//          } logo" class="logo" />
+//             <div class="text">
+//                <h2>${extension.name}</h2>
+//                <p>${extension.description}</p>
+//             </div>
+//          </div>
+//          <div class="second-part">
+//             <button class="btn-remove">Remove</button>
+//             <label class="switch">
+//                <input type="checkbox" ${extension.isActive ? "checked" : ""} />
+//                <span class="slider round" tabindex="0"></span>
+//             </label>
+//          </div>
+
+//             `;
+
+//          container.appendChild(card);
+//       });
+//    })
+//    .catch((err) => console.error("Failed to load JSON", err));
 
 // toggle dark mode and light mode
 let darkMode = localStorage.getItem("dark-mode");
@@ -67,4 +119,6 @@ function activateButton(button) {
    });
 
    button.classList.add("active");
+
+   getDynamicData(button.value);
 }
